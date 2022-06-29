c5f96fa18f4f16cdddc85faad072a0d6120fa026
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2022-06-29 14:54:09 +0900

    tyousei

--- app/models/family.rb
+++ app/models/family.rb
@@ -2,4 +2,6 @@ class Family < ApplicationRecord
     has_one_attached :image
     validates :family_name, presence: true 
     validates :image, presence: true 
+    validates :family_pr, presence: true 
+    validates :point, presence: true 
 end
